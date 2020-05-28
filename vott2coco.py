d3be8fa5fe045518f504907224bf1f62aa6c25d9
kimberly <kk316497@gmail.com>
2020-05-28 17:36:49 +0900

    make annotation files with vott files

--- vott2coco.py
+++ vott2coco.py
@@ -16,19 +16,16 @@ def check_image(file_path, height, width):
     return True
 
 
-def main(argv):
-    root_path = argv[1]
-    vott_path = os.path.join(root_path, 'train')
-
-    if not os.path.exists(root_path):
-        print('input path does not exit.')
-        return
-
-    output_dict = {}
+def vott2coco(vott_path, input_image_folder, json_path):
+    data_dict = {}
     images = []
     annotations = []
+    count = 0
     category_dict = {'home': 1, 'back': 2, 'hamburgermenu': 3, 'clickable': 4, 'more': 5, 'spinner': 6, 'search': 7,
                      'swipe': 8, 'edittext': 9, 'checkbox': 10, 'radio': 11, 'switch': 12, 'keyboard': 13}
+    category_count = []
+    for _ in category_dict:
+        category_count.append(0)
     categories = [{"supercategory": "change_page", "id": 1, "name": "home"},
                   {"supercategory": "change_page", "id": 2, "name": "back"},
                   {"supercategory": "change_page", "id": 3, "name": "hamburgermenu"},
@@ -43,15 +40,16 @@ def main(argv):
                   {"supercategory": "not_change_page", "id": 12, "name": "switch"},
                   {"supercategory": "not_change_page", "id": 13, "name": "keyboard"}]
 
-    annotation_index = 0
-    for image_id, json_file in enumerate(os.listdir(vott_path)):
+    annotation_index = 1
+    path = os.listdir(vott_path)
+    for image_id, json_file in enumerate(path):
         with open(os.path.join(vott_path, json_file), "r+") as fp:
             modified = False
             data = json.load(fp)
             height = data['asset']['size']['height']
             width = data['asset']['size']['width']
             file_name = data['asset']['name']
-            image_path = os.path.join(os.path.join(root_path, 'Object_Detection'), file_name)
+            image_path = os.path.join(input_image_folder, file_name)
             if not check_image(image_path, height, width):
                 continue
             image_data = {
@@ -59,20 +57,23 @@ def main(argv):
                 'width': width,
                 'id': image_id,
                 'file_name': file_name
-
             }
-            images.append(image_data)
+            box_dict_per_image = []
             box_list = data['regions']
             for box in box_list:
                 tag = box['tags']
                 if len(tag) != 1:
                     a = 1
                 tag = tag[0].lower()
+                # if tag != 'clickable':
+                #     continue
                 match = True
                 if tag not in category_dict:
                     match = False
 
                 if not match:
+                    if tag != 'clickable':
+                        continue
                     if tag == 'hambergermenu':
                         modified_tag = 'HamburgerMenu'
                         box['tags'][0] = modified_tag
@@ -81,31 +82,55 @@ def main(argv):
                     # ignore the tag 'ad'
                     elif tag == 'ad':
                         continue
-
+                    else:
+                        raise Exception()
+                category_id = category_dict[tag]
+                category_count[category_id - 1] += 1
+                count += 1
                 box_dict = {
                     'image_id': image_id,
                     'area': box['boundingBox']['height'] * box['boundingBox']['width'],
                     "iscrowd": 0,
                     'bbox': [
-                        box['boundingBox']['height'],
-                        box['boundingBox']['width'],
                         box['boundingBox']['left'],
-                        box['boundingBox']['top']
+                        box['boundingBox']['top'],
+                        box['boundingBox']['width'],
+                        box['boundingBox']['height']
                     ],
-                    'category_id': category_dict[tag],
+                    'category_id': category_id,
                     'id': annotation_index
                 }
-                annotations.append(box_dict)
                 annotation_index += 1
+                annotations.append(box_dict)
             if modified:
                 fp.seek(0)
                 json.dump(data, fp)
                 fp.truncate()
-    output_dict['images'] = images
-    output_dict['categories'] = categories
-    output_dict['annotations'] = annotations
-    with open('/home/kimberly/projects/instances_train2017.json', 'w') as fp:
-        json.dump(output_dict, fp)
+
+            images.append(image_data)
+            count += 1
+
+    data_dict['images'] = images
+    data_dict['categories'] = categories
+    data_dict['annotations'] = annotations
+    with open(json_path, 'w') as fp:
+        json.dump(data_dict, fp, indent=4)
+    print('total count: ' + str(count))
+    print(category_dict)
+    print('category count: ' + str(category_count))
+
+
+def main(argv):
+    root_path = argv[1]
+    vott_path = os.path.join(root_path, 'test_vott')
+    json_path = os.path.join(root_path, 'test.json')
+    input_image_folder = os.path.join(root_path, 'test')
+
+    if not os.path.exists(root_path):
+        print('input path does not exit.')
+        return
+
+    vott2coco(vott_path, input_image_folder, json_path)
 
 
 if __name__ == '__main__':
